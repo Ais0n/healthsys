3fbaef971bc696a7c1414a9c122a4a161db17adb
Ais0n <aisooon@yahoo.com>
2021-05-02 11:50:01 +0800

    demo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 ﻿import logo from './icon.png';
-import './App.less';
-import { Typography } from 'antd';
+import './App.css';
+import { Typography, Button, Input } from 'antd';
 
 const { Title } = Typography;
+const { Search} = Input;
+const onsearch = () => {
+  console.log("search")
+}
 
 function App() {
   return (
     <div className="App">
       <div className="header">
         <img className="logo" src={logo} alt=""></img>
-        <Title level="h1">阳光医疗服务平台</Title>
+        <div className="title"><Title level="h2">阳光医疗服务平台</Title></div>
+        <Search placeholder="input search text" onSearch={onsearch} style={{ width: 200 }} />
+        <Button type="primary">登录</Button>
+        <Button>注册</Button>
       </div>
       <footer> This is the game for the bootcamp in ByteDance</footer>
     </div>
