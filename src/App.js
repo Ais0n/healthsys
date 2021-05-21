fc4acc49fbeace24d8063734b4a936160343942f
LahElr <lahelr87@gmail.com>
2021-05-22 00:07:26 +0800

    login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Mainpage from './pages/Mainpage'
 import Guahao from './pages/Guahao'
 import Login from './pages/Login'
+import LoginSucceed from './pages/LoginSucceed'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" component={Mainpage}/>
         <Route path="/guahao" component={Guahao}/>
         <Route path="/login" component={Login}/>
+        <Route path="/loginsucceed" component={LoginSucceed}/>
       </div>
     </BrowserRouter>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,33 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState , Row} from 'react';
 import { Typography, Button, Input, Form, Checkbox} from 'antd';
+import { Layout } from 'antd'
 import Myheader from './Components/Myheader'
 import Navbar from './Components/Navbar'
+import Verifycode from './Components/Verifycode'
+import './Login.css'
 
 const { Title } = Typography;
 const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
+    labelCol: { span: 9 },
+    wrapperCol: { span: 6 },
 };
 const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
+    wrapperCol: { offset: 9, span: 6 },
+};
+const submitLayout = {
+    wrapperCol: {align: "center"},
+    // offset: 11, span: 6, 
+};
+const varifyCodeLayout = {
+    wrapperCol: {align: "center"},
 };
 
 class Login extends React.Component {
     constructor(props){
         super(props);
     }
+
+    verifyCodeSucceed = false;
+
     onFinish = (values) => {
+        //此处应有验证码的处理
         console.log('Success:', values);
+        // alert("success!");
+        alert(this.verifyCodeSucceed);//调用这个东西来获取验证码是否已经划过了
+        if(this.verifyCodeSucceed){
+            this.props.history.push({
+                pathname:"/loginsucceed",
+                query:{
+                    // username: values.username,
+                    // password: values.password,
+                    // remember: values.remember
+                    values:values
+                }
+            });
+        }
+        else{
+            alert("请通过验证码确认")
+        }
+        
     };
 
     onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
+        alert("fail!");
     };
+    getVerifyCodeSuccess = (verifyCodeComponent,success) => {
+        this.verifyCodeSucceed = success;
+    }
     render(){
         return(
             <div>
                 <Myheader/>
                 <Navbar/>
+                <div  className="loginForm">
                 <Form
                 {...layout}
                 name="basic"
@@ -36,31 +72,38 @@ class Login extends React.Component {
                 onFinishFailed={this.onFinishFailed}
                 >
                 <Form.Item
-                    label="Username"
+                    label="用户名"
                     name="username"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
+                    rules={[{ required: true, message: '请输入用户名' }]}
                 >
                     <Input />
                 </Form.Item>
 
                 <Form.Item
-                    label="Password"
+                    label="密码"
                     name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
+                    rules={[{ required: true, message: '请输入密码' }]}
                 >
                     <Input.Password />
                 </Form.Item>
 
                 <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-                    <Checkbox>Remember me</Checkbox>
+                    <Checkbox>记住我</Checkbox>
                 </Form.Item>
 
-                <Form.Item {...tailLayout}>
-                    <Button type="primary" htmlType="submit">
-                    Submit
+                <Form.Item {...varifyCodeLayout}>
+                    <Verifycode caller={this}/>
+                </Form.Item>
+
+                <Form.Item {...submitLayout}>
+                    <Button type="primary" htmlType="完成" align="center">
+                    好了
                     </Button>
                 </Form.Item>
+
                 </Form>
+                
+                </div>
             </div>
         );
     };
