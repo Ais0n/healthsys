62874efe08dc297e1dfd702859e4260153dc833f
Ais0n <aisooon@yahoo.com>
2021-06-12 12:01:05 +0800

    feat: add 404 route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 ﻿import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 import Mainpage from './pages/Mainpage'
 import Guahao from './pages/Guahao'
 import Login from './pages/Login'
@@ -7,11 +7,12 @@ import Register from './pages/Register'
 import LoginSucceed from './pages/LoginSucceed'
 import RegisterSucceed from './pages/RegSucceed'
 import User from './pages/User'
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <Switch>
         <Route exact path="/" component={Mainpage}/>
         <Route path="/guahao" component={Guahao}/>
         <Route path="/login" component={Login}/>
@@ -19,7 +20,8 @@ function App() {
         <Route path="/loginsucceed" component={LoginSucceed}/>
         <Route path="/regsucceed" component={RegisterSucceed}/>
         <Route path="/user" component={User}/>
-      </div>
+        <Route path="/:location" component={ErrorPage}/>
+      </Switch>
     </BrowserRouter>
   );
 }
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Sidebar from './Components/Sidebar';
+import { Layout, Menu, Breadcrumb, Card, Button, Form, Input, Radio, Checkbox, Select, Result} from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined, LockOutlined} from '@ant-design/icons';
+
+import Myheader from './Components/Myheader'
+import Navbar from './Components/Navbar'
+import './User.css'
+import ChangePassword from './UserManage/ChangePassword';
+import ChangeUserData from './UserManage/ChangeUserData';
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+class ErrorPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+                <Myheader />
+                <Navbar />
+                <div className="mainContent">
+                    <Result
+                        status="404"
+                        title="404"
+                        subTitle="您所访问的页面不存在"
+                    />
+                </div>
+            </>
+        );
+    }
+}
+
+export default ErrorPage;
