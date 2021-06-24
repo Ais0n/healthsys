6080e5b6112f3c1e6e69fccff7650a22bf4da566
Ais0n <aisooon@yahoo.com>
2021-06-24 23:44:45 +0800

    feat: 添加跳转

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import shuffle_pic_1 from '../pic/shuffle_pic_1.png' 
 import patient_pic from '../pic/I_am_patient.png' 
 import doctor_pic from '../pic/I_am_doctor.png' 
@@ -19,167 +20,148 @@ import Myheader from './Components/Myheader'
 import Navbar from './Components/Navbar'
 import Myfooter from './Components/Myfooter'
 
+
 const { Title } = Typography;
 
-const onchange = () => {
-    console.log("search")
+class Mainpage extends React.Component {
+
+  handleBook = () => {
+    this.props.history.push({
+      pathname:"/guahao",
+    });
+  }
+
+  render(){
     return (
-    <div className="doctor-info-table">
-      <div className="doctor-info">
-          <img className="doctor-profile" src={doctor_profile} alt=""></img>
-          <div className="doctor-info-1">张三</div>
-          <div className="doctor-info-2">神经内科 主治医师</div>
-      </div>
-      <div className="doctor-info">
-          <img className="doctor-profile" src={doctor_profile} alt=""></img>
-          <div className="doctor-info-1">李四</div>
-          <div className="doctor-info-2">骨科 专家医师</div>
-      </div>
-      <div className="doctor-info">
-          <img className="doctor-profile" src={doctor_profile} alt=""></img>
-          <div className="doctor-info-1">王五</div>
-          <div className="doctor-info-2">口腔科 专家医师</div>
-      </div>
-    </div>
-    )
-}
-
-function test(){
-  return (
-    <div>Hello, world </div>
-  );
-}
-
-function Mainpage() {
-  return (
-    <div className="App">
-      <Myheader/>
-      <Navbar/>
-      <div className="picture">
-          <div className="sub-picture">
-            <Carousel autoplay>
-                <div>
-                  <img className="shuffle-pic" src={shuffle_pic_1} alt=""></img>
-                </div>
-                <div>
-                  <img className="shuffle-pic" src={shuffle_pic_1} alt=""></img>
-                </div>
-                <div>
-                  <img className="shuffle-pic" src={shuffle_pic_1} alt=""></img>
-                </div>
-                <div>
-                  <img className="shuffle-pic" src={shuffle_pic_1} alt=""></img>
-                </div>
-              </Carousel>
-          </div>
-
-          <div className="sub-picture">
-            <a href="#">
-            <div className="doctor-patient">
-                <img className="doctor-patient-pic" src={patient_pic} alt=""></img>
-                <div className="doctor-patient-text-1">我是患者</div>
-                <div className="doctor-patient-text-2">I'm a patient</div>
+      <div className="App">
+        <Myheader/>
+        <Navbar/>
+        <div className="picture">
+            <div className="sub-picture">
+              <Carousel autoplay>
+                  <div>
+                    <img className="shuffle-pic" src={shuffle_pic_1} alt=""></img>
+                  </div>
+                  <div>
+                    <img className="shuffle-pic" src={shuffle_pic_1} alt=""></img>
+                  </div>
+                  <div>
+                    <img className="shuffle-pic" src={shuffle_pic_1} alt=""></img>
+                  </div>
+                  <div>
+                    <img className="shuffle-pic" src={shuffle_pic_1} alt=""></img>
+                  </div>
+                </Carousel>
             </div>
-            </a>
 
-            <a href="#">
-            <div className="doctor-patient">
-                <img className="doctor-patient-pic" src={doctor_pic} alt=""></img>
-                <div className="doctor-patient-text-1">我是医生</div>
-                <div className="doctor-patient-text-2">I'm a doctor</div>
+            <div className="sub-picture">
+              <a href="#">
+              <div className="doctor-patient">
+                  <img className="doctor-patient-pic" src={patient_pic} alt=""></img>
+                  <div className="doctor-patient-text-1">我是患者</div>
+                  <div className="doctor-patient-text-2">I'm a patient</div>
+              </div>
+              </a>
+
+              <a href="#">
+              <div className="doctor-patient">
+                  <img className="doctor-patient-pic" src={doctor_pic} alt=""></img>
+                  <div className="doctor-patient-text-1">我是医生</div>
+                  <div className="doctor-patient-text-2">I'm a doctor</div>
+              </div>
+              </a>
             </div>
-            </a>
-          </div>
+        </div>
+
+
+        <div className="text">
+            <div className="sub-text">
+                <div className="sub-text-title">
+                  <div className="sub-title">找科室</div>
+                  <div className="sub-sub-title">选择您要就诊的科室</div>
+                </div>
+                <div className="department-table">
+                  <a href="/user">
+                  <div className="department">
+                      <img className="depart-pic" src={depart_pic_A} alt=""></img>
+                      <div className="depart-text">精神卫生科</div>
+                  </div>
+                  </a>
+
+                  <a href="#">
+                  <div className="department">
+                      <img className="depart-pic" src={depart_pic_B} alt=""></img>
+                      <div className="depart-text">内分泌科</div>
+                  </div>
+                  </a>
+
+                  <a href="#">
+                  <div className="department">
+                      <img className="depart-pic" src={depart_pic_C} alt=""></img>
+                      <div className="depart-text">肾脏病科</div>
+                  </div>
+                  </a>
+
+                  <a href="#">
+                  <div className="department">
+                      <img className="depart-pic" src={depart_pic_D} alt=""></img>
+                      <div className="depart-text">神经内科</div>
+                  </div>
+                  </a>
+
+                  <a href="#">
+                  <div className="department">
+                      <img className="depart-pic" src={depart_pic_E} alt=""></img>
+                      <div className="depart-text">心血管内科</div>
+                  </div>
+                  </a>
+
+                  <a href="#">
+                  <div className="department">
+                      <img className="depart-pic" src={more} alt=""></img>
+                      <div className="depart-text">更多</div>
+                  </div>
+                  </a>
+
+                </div>
+            </div>
+
+            <div className="vertical-split-line-3" />
+
+            <div className="sub-text">
+                <div className="sub-text-title">
+                  <div className="sub-title">找名医</div>
+                  <div className="sub-sub-title">提前预约，足不出户</div>
+                </div>             
+
+                <div className="doctor-info-table">
+                  <Space direction="horizontal" size="large">
+                  <div className="doctor-info">
+                      <img className="doctor-profile" src={doc1} alt=""></img>
+                      <div className="doctor-info-1">张三</div>
+                      <div className="doctor-info-2">神经内科 主治医师</div>
+                      <Button type="primary" onClick={this.handleBook}>立即预约</Button>
+                  </div>
+                  <div className="doctor-info">
+                      <img className="doctor-profile" src={doc2} alt=""></img>
+                      <div className="doctor-info-1">李四</div>
+                      <div className="doctor-info-2">骨科 专家医师</div>
+                      <Button type="primary" onClick={this.handleBook}>立即预约</Button>
+                  </div>
+                  <div className="doctor-info">
+                      <img className="doctor-profile" src={doc3} alt=""></img>
+                      <div className="doctor-info-1">王五</div>
+                      <div className="doctor-info-2">口腔科 专家医师</div>
+                      <Button type="primary" onClick={this.handleBook}>立即预约</Button>
+                  </div>
+                  </Space>
+                </div>
+            </div>
+        </div>
+        <Myfooter/>
       </div>
-
-
-      <div className="text">
-          <div className="sub-text">
-              <div className="sub-text-title">
-                <div className="sub-title">找科室</div>
-                <div className="sub-sub-title">选择您要就诊的科室</div>
-              </div>
-              <div className="department-table">
-                <a href="/user">
-                <div className="department">
-                    <img className="depart-pic" src={depart_pic_A} alt=""></img>
-                    <div className="depart-text">精神卫生科</div>
-                </div>
-                </a>
-
-                <a href="#">
-                <div className="department">
-                    <img className="depart-pic" src={depart_pic_B} alt=""></img>
-                    <div className="depart-text">内分泌科</div>
-                </div>
-                </a>
-
-                <a href="#">
-                <div className="department">
-                    <img className="depart-pic" src={depart_pic_C} alt=""></img>
-                    <div className="depart-text">肾脏病科</div>
-                </div>
-                </a>
-
-                <a href="#">
-                <div className="department">
-                    <img className="depart-pic" src={depart_pic_D} alt=""></img>
-                    <div className="depart-text">神经内科</div>
-                </div>
-                </a>
-
-                <a href="#">
-                <div className="department">
-                    <img className="depart-pic" src={depart_pic_E} alt=""></img>
-                    <div className="depart-text">心血管内科</div>
-                </div>
-                </a>
-
-                <a href="#">
-                <div className="department">
-                    <img className="depart-pic" src={more} alt=""></img>
-                    <div className="depart-text">更多</div>
-                </div>
-                </a>
-
-              </div>
-          </div>
-
-          <div className="vertical-split-line-3" />
-
-          <div className="sub-text">
-              <div className="sub-text-title">
-                <div className="sub-title">找名医</div>
-                <div className="sub-sub-title">提前预约，足不出户</div>
-              </div>             
-
-              <div className="doctor-info-table">
-                <Space direction="horizontal" size="large">
-                <div className="doctor-info">
-                    <img className="doctor-profile" src={doc1} alt=""></img>
-                    <div className="doctor-info-1">张三</div>
-                    <div className="doctor-info-2">神经内科 主治医师</div>
-                    <Button type="primary">立即预约</Button>
-                </div>
-                <div className="doctor-info">
-                    <img className="doctor-profile" src={doc2} alt=""></img>
-                    <div className="doctor-info-1">李四</div>
-                    <div className="doctor-info-2">骨科 专家医师</div>
-                    <Button type="primary">立即预约</Button>
-                </div>
-                <div className="doctor-info">
-                    <img className="doctor-profile" src={doc3} alt=""></img>
-                    <div className="doctor-info-1">王五</div>
-                    <div className="doctor-info-2">口腔科 专家医师</div>
-                    <Button type="primary">立即预约</Button>
-                </div>
-                </Space>
-              </div>
-          </div>
-      </div>
-      <Myfooter/>
-    </div>
-  );
+    );
+  }
 }
 
 export default Mainpage;
