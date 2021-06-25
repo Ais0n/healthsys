718c1a0661bff08e4e264ff9a87471b0cf1b027c
Ais0n <aisooon@yahoo.com>
2021-06-25 11:46:02 +0800

    feat: 修改navbar

--- src/pages/Components/Navbar.js
+++ src/pages/Components/Navbar.js
@@ -8,9 +8,8 @@ export default function Navbar(){
             <NavLink to="/" className="sub-navigation">首页</NavLink>
             <NavLink to="/guahao" className="sub-navigation">挂号</NavLink>
             <NavLink to="/chatroomloading" className="sub-navigation">在线问诊</NavLink>
-            <NavLink to="/guahao" className="sub-navigation">送药上门</NavLink>
-            <NavLink to="/user" className="sub-navigation">个人中心</NavLink>
             <NavLink to="/keshiinfo" className="sub-navigation">科室介绍</NavLink>
+            <NavLink to="/user" className="sub-navigation">个人中心</NavLink>
         </div>
     )
 };
